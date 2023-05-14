2f942297d95040fbe6f416fe1343fbeb418bee0f
anilasa <anilasa@gmail.com>
2023-05-14 05:56:43 -0400

    create-react-app example

---
+++ src/components/Todos.test.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { render, waitFor, screen } from "@testing-library/react";
+import Todos from "../components/Todos";
+
+
+jest.mock("axios");
+
+const dummyTodos = [
+    {
+        userId: 1,
+        id: 1,
+        title: "todo 1",
+        completed: false,
+    },
+    {
+        userId: 1,
+        id: 2,
+        title: "todo 2",
+        completed: false,
+    },
+    {
+        userId: 1,
+        id: 3,
+        title: "todo 3",
+        completed: false,
+    },
+];
+
+test("todos list", async () => {
+    axios.get.mockResolvedValue({ data: dummyTodos });
+    render(<Todos />);
+
+    const todoList = await waitFor(() => screen.findAllByTestId("todo"));
+
+    expect(todoList).toHaveLength(3);
+});
